1655c80f4f831429e7f9f08c60ccb241a9182d5c
bradtimmis <bradleytimmis@gmail.com>
2021-09-19 11:41:25 -0400

    feat: add schema for channel object This is the first creation of the object, and is subject to change. The main Channel variables are created from this table: https://discord.com/developers/docs/resources/channel#channel-object-channel-structure

---
+++ yadw/resources/channel.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+__all__ = ("Channel",)
+
+
+class Channel(BaseModel):
+    id: int
+    type: int
+    guild_id: Optional[int]
+    position: Optional[int]
+    permission_overwrites: Optional[list]
+    name: Optional[str]
+    topic: Optional[str]
+    nsfw: Optional[bool]
+    last_message_id: Optional[int]
+    bitrate: Optional[int]
+    user_limit: Optional[int]
+    rate_limit_per_user: Optional[int]
+    recipients: Optional[list]
+    icon: Optional[str]
+    owner_id: Optional[int]
+    application_id: Optional[int]
+    parent_id: Optional[int]
+    last_pin_timestamp: Optional[str]
+    rtc_region: Optional[str]
+    video_quality_mode: Optional[int]
+    message_count: Optional[int]
+    member_count: Optional[int]
+    # thread_metadata:
+    # member:
+    default_auto_archive_duration: Optional[int]
+    permissions: Optional[str]
